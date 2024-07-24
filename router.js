2c8ee86b07c3a9973385b3553830d96c5e3759a7
HIsheR <shawn1440982358@163.com>
2024-07-24 21:15:24 +0800

    feat: 修改开发环境的配置

--- router.js
+++ router.js
@@ -3,15 +3,12 @@ import Vue from 'vue';//加载全局组件时,都需要引入Vue
 import Router from 'vue-router';//引入vue-router
 //引入在路由中需要用到的组件
 import Home from '@/pages/index/index';
-import Example from '@/pages/example/index';
 
 //第二步:加载Router
 Vue.use(Router);//加载全局组件Router
 
 //第三步:配置路由实例
 export default new Router({
-  //mode:'history', //路由模式:默认为hash,如果改为history,则需要后端进行配合
-  //base:'/',//基路径:默认值为'/'.如果整个单页应用在/app/下,base就应该设为'/app/'.一般可以写成__dirname,在webpack中配置.
   routes: [
     {
       path: '/',
